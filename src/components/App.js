239c0f8040dad94f206607dde1edffb4a9feaf72
chiraggarg025 <chiraggarg025@gmail.com>
2020-10-21 16:13:25 +0530

    add rect router 2 and login form ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { fetchPosts } from "../actions/posts";
-import { PostsList, Navbar } from "./";
+import { Home, Navbar, Page404,Login } from "./";
 
-const Login = () => <div>Login</div>;
 
 const SignUp = () => <div>SignUp</div>;
-
-const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -23,21 +20,18 @@ class App extends React.Component {
         <div>
           <Navbar />
           {/* <PostsList posts={posts} /> */}
-          <ul>
-            <li>
-              <Link to="/">Home </Link>
-            </li>
-            <li>
-              <Link to="/login">Login </Link>
-            </li>
-            <li>
-              <Link to="/signup">SignUp </Link>
-            </li>
-          </ul>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => {
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route component={Page404} />
+          </Switch>
         </div>
       </Router>
     );
