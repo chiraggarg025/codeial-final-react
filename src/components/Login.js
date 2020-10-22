1bf86593743272e59427ae8a5bb23567340f995d
chiraggarg025 <chiraggarg025@gmail.com>
2020-10-22 12:03:07 +0530

    Form handling: Controlled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
-    constructor (props){
-        super(props);
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+  constructor(props) {
+    super(props);
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-    }
-    handleFormSubmit = (e) =>{
-        e.preventDefault();
-        console.log('Email Ref',this.emailInputRef)
-        console.log('Password Ref',this.passwordInputRef)
-    }
-    render() {
-        return (
-            <form className="login-form">
-                <span className="login-signup-header">Log In</span>
-                <div className="field">
-                    <input type="email" placeholder="Email" required ref = {this.emailInputRef} />
-                </div>
-                <div className="field">
-                    <input type="password" placeholder="Password" required ref = {this.passwordInputRef}/>
-                </div>
-                <div className="field">
-                    <button onClick={this.handleFormSubmit}>Login</button>
-                </div>
-            </form>
-        );
-    }
+  handleEmailChange = (e) =>{
+    //   console.log(e.target.value);
+      this.setState({
+          email:e.target.value
+      })
+  }
+  handlePasswordChange = (e) =>{
+    // console.log(e.target.value);
+    this.setState({
+        password:e.target.value
+    })
+}
+
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    // console.log("Email Ref", this.emailInputRef);
+    // console.log("Password Ref", this.passwordInputRef);
+    console.log('THIS>STATE', this.state)
+  };
+  render() {
+    return (
+      <form className="login-form">
+        <span className="login-signup-header">Log In</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
+        </div>
+        <div className="field">
+          <button onClick={this.handleFormSubmit}>Login</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Login;
